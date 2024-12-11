17b48e6652a21f9cd5361c4cae62c85d7b714a62
zeyuzhang <wfzhangzeyu@163.com>
2024-12-11 20:21:13 +0800

    Fix the bug of dialogue agents in run_dialogue.py

--- run_agent_samples/run_dialogue.py
+++ run_agent_samples/run_dialogue.py
@@ -45,12 +45,12 @@ class LLM():
 # ----- Dialogue Agent -----
 
 class DialogueAgent():
-    def __init__(self, role):
+    def __init__(self, role, another_role):
         self.llm = LLM()
 
         self.role = role
+        self.another_role = another_role
         self.memory = FUMemory(MemoryConfig(DialogueAgentMemoryConfig))
-        
     
     def response(self, observation):
         prompt = PromptTemplate(
@@ -58,12 +58,13 @@ class DialogueAgent():
                 template= DialogueAgentPrompt,
             ).format(role = self.role, memory_context = self.memory.recall(observation), observation = observation)
         res = self.llm.fast_run(prompt)
+        self.memory.store('%s: %s\n%s: %s' % (self.another_role, observation, self.role, res))
         return res
 
 
 def run_personal_assistant():
-    user = DialogueAgent('User')
-    assistant = DialogueAgent('Assistant')
+    user = DialogueAgent('User', 'Assistant')
+    assistant = DialogueAgent('Assistant', 'User')
     assistant_response = assistant.response('Please start the dialogue between User and Assistant.')
 
     for current_step in range(MAX_STEP):
