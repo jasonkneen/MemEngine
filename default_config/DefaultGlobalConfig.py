b4dbbabfca60574820cb6df0405a11aca6b0e141
zeyuzhang <wfzhangzeyu@163.com>
2024-12-16 16:19:37 +0800

    Change Config.

--- default_config/DefaultGlobalConfig.py
+++ default_config/DefaultGlobalConfig.py
@@ -2,8 +2,8 @@ DEFAULT_GLOBAL_CONFIG = {
     'usable_gpu': '3'
 }
 
-DEFAULT_OPENAI_APIKEY = 'sk-02otYN1Q5IcaC3kewSHonCROpAJCcdRSJ8u7mlvjLT6GY1Ut'
-DEFAULT_OPENAI_APIBASE = 'https://api.chatanywhere.tech/v1'
+DEFAULT_OPENAI_APIKEY = '[YOUR_API KEY]'
+DEFAULT_OPENAI_APIBASE = '[YOUR_API_BASE]'
 
-DEFAULT_LLAMA3_8B_INSTRUCT_PATH = '/data/zhangzeyu/local_llms/Llama-3-8B-Instruct'
-DEFAULT_E5_BASE_V2_PATH = '/data/zhangzeyu/local_llms/e5-base-v2'
+DEFAULT_LLAMA3_8B_INSTRUCT_PATH = '[YOUR_PATH]'
+DEFAULT_E5_BASE_V2_PATH = '[YOUR_PATH]'
